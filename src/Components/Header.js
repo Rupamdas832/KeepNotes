947f85f3b44bb6f8b5b086354346b472660ff32f
Rupam <rupamdas832@gmail.com>
2021-03-01 21:03:27 +0530

    bookmark functionality started

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,6 +4,7 @@ function Header() {
     return (
         <div className="header">
             <h1>Keep Shorts</h1>
+            <h4 style={{color: "black"}}>Save notes, quotes, list</h4>
         </div>
     )
 }
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,12 +1,26 @@
 import React, { useState } from 'react'
 import {v4} from "uuid"
+import Modal from "react-modal"
 import {TiPinOutline, TiPin} from "react-icons/ti"
+import {VscBookmark} from "react-icons/vsc"
 
 function Input({setKeep}) {
     const [title, setTitle] = useState("")
     const [desc, setDesc] = useState("")
     const [isPin, setIsPin] = useState(false)
     const [color, setColor] = useState("")
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+    const customStyles = {
+        content : {
+          top                   : '50%',
+          left                  : '50%',
+          right                 : 'auto',
+          bottom                : 'auto',
+          marginRight           : '-50%',
+          transform             : 'translate(-50%, -50%)'
+        }
+      };
 
     const addBtn = () => {
         const item = {
@@ -22,6 +36,9 @@ function Input({setKeep}) {
         setIsPin("")
         setColor("")
     }
+    const openModal = () => {
+        setModalIsOpen(true)
+    }
     return (
         <div className="inputSection">
         <div className="inputCard" style={{backgroundColor: `${color}`}}>
@@ -31,7 +48,14 @@ function Input({setKeep}) {
             </div>
             <input placeholder="Type a note..." value={desc} onChange={(e) => setDesc(e.target.value)} className="inputField desc" style={{backgroundColor: `${color}`}}/>
             <div className="inputActionBtns">
-                <input type="color" id="favcolor" name="favcolor" value="#e76f51" onChange={(e) => setColor(e.target.value)}/>
+                <div>
+                    <input type="color" id="favcolor" name="favcolor" value="#e76f51" onChange={(e) => setColor(e.target.value)}/>
+                    <button className="btn" style={{fontSize: "1.3rem", marginLeft: "1rem"}} onClick={openModal}><VscBookmark/></button>
+                    <Modal isOpen={modalIsOpen} style={customStyles}>
+                    <h1>I m modal</h1>
+                    <button onClick={() => setModalIsOpen(false)}>Close</button>
+                    </Modal>
+                </div>
                 <button onClick={addBtn} className="btn">Done</button>
             </div>
         </div>       
